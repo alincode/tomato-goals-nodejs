bfb187f22d665eb2081b175575a66e6e93281c74
alincode <alin.code@gmail.com>
2015-11-25 21:42:12 +0800

    improve to be OOP style

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,33 +1,39 @@
-function getminuteFromNow(minute) {
-  return new Date(new Date().valueOf() + minute * 60 * 1000);
+var tomatoGoals = {};
+
+tomatoGoals.countdown = {
+  clock: $('#clock'),
+  init: function() {
+    this._setClockFormat();
+    $('#pomodoro').click({minute:25}, this.handler._countdown);
+    $('#short-break').click({minute:5}, this.handler._countdown);
+    $('#long-break').click({minute:10}, this.handler._countdown);
+    $('#reset').click({minute:0}, this.handler._countdown);
+
+    $('#start').click({commend:'resume'}, this.handler._commend);
+    $('#stop').click({commend:'pause'}, this.handler._commend);
+  },
+  _setClockFormat: function() {
+    var minuteDate = this._getminuteFromNow(0);
+    $(clock).countdown(minuteDate, function(event) {
+      var time = event.strftime('%H:%M:%S');
+      $(this).html(time);
+    });
+  },
+  _getminuteFromNow: function(minute) {
+    return new Date(new Date().valueOf() + minute * 60 * 1000);
+  },
+  handler : {
+    _commend: function(eventObject) {
+      console.log('commend:' + eventObject.data.commend);
+      $(clock).countdown(eventObject.data.commend);
+    },
+    _countdown: function(eventObject) {
+      console.log('minute:' + eventObject.data.minute);
+      $(clock).countdown(tomatoGoals.countdown._getminuteFromNow(eventObject.data.minute));
+    }
+  }
 }
 
-var $clock = $('#clock');
-
-$clock.countdown(getminuteFromNow(0), function(event) {
-  $(this).html(event.strftime('%H:%M:%S'));
-});
-
-$('#pomodoro').click(function() {
-  $clock.countdown(getminuteFromNow(25));
-});
-
-$('#short-break').click(function() {
-  $clock.countdown(getminuteFromNow(5));
-});
-
-$('#long-break').click(function() {
-  $clock.countdown(getminuteFromNow(10));
-});
-
-$('#stop').click(function() {
-  $clock.countdown('pause');
-});
-
-$('#start').click(function() {
-  $clock.countdown('resume');
-});
-
-$('#reset').click(function() {
-  $clock.countdown(getminuteFromNow(0));
+$(document).ready(function() {
+  tomatoGoals.countdown.init();
 });
