a5b4bdecb9b6d8585cc09fd7219de825cb0b8003
alincode <alin.code@gmail.com>
2015-11-26 14:44:01 +0800

    improve reset button

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -9,10 +9,10 @@ tomatoGoals.timer = {
     this._setClockFormat();
     this.myCountDown.on('finish.countdown', this.handler._finishCountdown);
 
-    $('#pomodoro').click({minute:1, type: 'tomato'}, this.handler._countdown);
-    $('#short-break').click({minute:1, type: 'break'}, this.handler._countdown);
+    $('#pomodoro').click({minute:25, type: 'tomato'}, this.handler._countdown);
+    $('#short-break').click({minute:5, type: 'break'}, this.handler._countdown);
     $('#long-break').click({minute:10, type: 'break'}, this.handler._countdown);
-    $('#reset').click({minute:0}, this.handler._countdown);
+    $('#reset').click({minute:0, type: 'reset'}, this.handler._countdown);
 
     $('#start').click({commend:'resume'}, this.handler._commend);
     $('#stop').click({commend:'pause'}, this.handler._commend);
@@ -27,6 +27,11 @@ tomatoGoals.timer = {
   _getminuteFromNow: function(minute) {
     return new Date(new Date().valueOf() + minute * 60 * 1000);
   },
+  _clear: function(){
+    console.log('clear current minute and type');
+    this.currentCountDownMinute = 0;
+    this.currentCountDownType = '';
+  },
   handler : {
     _commend: function(eventObject) {
       console.log('commend:' + eventObject.data.commend);
@@ -35,11 +40,18 @@ tomatoGoals.timer = {
     },
     _countdown: function(eventObject) {
       var minute = eventObject.data.minute;
+      var type = eventObject.data.type;
+
       var that = tomatoGoals.timer;
       that.currentCountDownMinute = minute;
       that.currentCountDownType = eventObject.data.type;
       console.log('minute:' + minute);
-      var countdown = that.myCountDown.countdown(that._getminuteFromNow(minute));
+      that.myCountDown.countdown(that._getminuteFromNow(minute));
+
+      if(type === 'reset'){
+        that._clear();
+        return;
+      }
     },
     _finishCountdown: function(event){
       // http://hilios.github.io/jQuery.countdown/documentation.html
@@ -84,9 +96,20 @@ tomatoGoals.history = {
   },
   updateNotifications: function(){
     console.log('update notifications');
-    var message = 'Total tomato time is ' + this.getItem(this.tomotoTimeKey) +
-      ' minutes , break time is ' + this.getItem(this.breakTimeKey) + ' minutes.';
+    var tomatoTime = this.getItem(this.tomotoTimeKey);
+    var breakTime = this.getItem(this.breakTimeKey);
+
+    if(tomatoTime == 0 && breakTime == 0){
+      return;
+    }
+
+    var message = 'Total tomato time is ' + tomatoTime +
+      ' minutes , break time is ' + breakTime + ' minutes.';
     $('#notifications-panel').html(message);
+  },
+  clear: function(){
+    this.setItem(this.breakTimeKey, 0);
+    this.setItem(this.tomotoTimeKey, 0);
   }
 }
 
